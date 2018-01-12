ae5d0592ac2da72fbf4bab80b3dcc27716831a89
Nathan <jcarra@student.42.fr>
2018-01-12 15:44:44 +0100

    Changes

--- New/get_next_line.c
+++ New/get_next_line.c
@@ -3,7 +3,6 @@
 //
 
 #include <stdlib.h>
-#include <unistd.h>
 #include "get_next_line.h"
 #include "libft.h"
 
@@ -23,12 +22,13 @@ int		ft_read(const int fd, char **buf)
 	char		read_buf[BUFF_SIZE + 1];
 	int			read_ret;
 
-	while ((read_ret = (int)read(fd, read_buf, BUFF_SIZE)) > 0)
-		ft_read_stock(buf, read_buf, read_ret);
-	if (read_ret == -1)
+	if ((read_ret = (int)read(fd, read_buf, BUFF_SIZE)) == -1)
 		return (-1);
 	if (read_ret)
+	{
 		ft_read_stock(buf, read_buf, read_ret);
+		return (1);
+	}
 	return (0);
 }
 
@@ -36,21 +36,28 @@ int		get_next_line(const int fd, char **line)
 {
 	static char	*buf = NULL;
 	char		*strchr_ret;
+	char		c;
+	int			ft_read_ret;
 
 	if (!line || fd < 0 || BUFF_SIZE <= 0)
 		return (-1);
-	if (!buf && ft_read(fd, &buf) == -1)
-		return (-1);
-	if (buf)
+	ft_read_ret = 1;
+	while (ft_read_ret == 1)
 	{
-		if (!(strchr_ret = ft_strchr(buf, '\n')))
-			strchr_ret = buf + ft_strlen(buf);
-		strchr_ret[0] = '\0';
-		*line = ft_strdup(buf);
-		buf = ft_strcpy(buf, strchr_ret + 1);
-		if (!ft_strlen(buf))
-			ft_strdel(&buf);
-		return (1);
+		if ((ft_read_ret = ft_read(fd, &buf)) == -1)
+			return (-1);
+		if (buf && ((strchr_ret = ft_strchr(buf, '\n')) || ft_read_ret == 0))
+		{
+			if (ft_read_ret == 0 && !strchr_ret)
+				strchr_ret = buf + ft_strlen(buf);
+			c = strchr_ret[0];
+			strchr_ret[0] = '\0';
+			*line = ft_strdup(buf);
+			buf = ft_strcpy(buf, (c == '\n') ? strchr_ret + 1 : strchr_ret);
+			if (!ft_strlen(buf))
+				ft_strdel(&buf);
+			return (1);
+		}
 	}
 	*line = NULL;
 	return (0);
